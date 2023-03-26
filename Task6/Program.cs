828b640f216a8f247195093d3284d63e64152601
Valeriy Ziborov <ziboroffvalera@gmail.com>
2023-03-26 11:24:13 +0300

    Initial commit

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число ");
+string NumOne = Console.ReadLine();
+int One = Convert.ToInt32(NumOne);
+
+if (One % 2 == 0) {
+    Console.WriteLine($"{One} является четным");
+}
+else {
+    Console.WriteLine($"{One} является нечетным");
+}
